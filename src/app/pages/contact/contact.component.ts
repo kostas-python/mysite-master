8166128c80247d1bc472f9a947dd3ace14a4b666
Kostas <diamantopoulos.kostas@hotmail.com>
2024-04-05 23:16:40 +0100

    added get in touch form

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {  RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { TopBarComponent } from 'src/app/layout/header/top-bar/top-bar.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [RouterLink, RouterOutlet,RouterModule],
+  imports: [RouterLink, RouterOutlet,RouterModule,TopBarComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })

4c6b19b8133b9df58d2878759fe92fce31496c3a
Kostas <diamantopoulos.kostas@hotmail.com>
2024-04-07 12:12:38 +0100

    contact comp improvements

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {  RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { TopBarComponent } from 'src/app/layout/header/top-bar/top-bar.component';
+
 
 @Component({
   selector: 'app-contact',
@@ -9,6 +10,23 @@ import { TopBarComponent } from 'src/app/layout/header/top-bar/top-bar.component
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
 
+export class ContactComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+    
+    const submitBtn = document.querySelector('.submit-btn') as HTMLInputElement;
+    submitBtn.addEventListener('click', this.onSubmit);
+  }
+
+  onSubmit(event: Event) {
+    event.preventDefault(); 
+    
+    console.log('Form submitted!');
+    
+    const nameInput = document.getElementById('name') as HTMLInputElement;
+    console.log('Name:', nameInput.value);
+    
+  }
 }

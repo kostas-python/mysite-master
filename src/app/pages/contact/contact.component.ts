52c8e365621359b2b317479ce1a2101782709f57
Kostas <diamantopoulos.kostas@hotmail.com>
2024-04-07 22:22:42 +0100

    contact comp improvements

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,32 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,} from '@angular/core';
 import {  RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { TopBarComponent } from 'src/app/layout/header/top-bar/top-bar.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
+
+
 
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [RouterLink, RouterOutlet,RouterModule,TopBarComponent],
+  imports: [RouterLink, RouterOutlet,RouterModule,
+    TopBarComponent, FormsModule,ReactiveFormsModule,HttpClientModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 
-export class ContactComponent implements OnInit {
-  constructor() { }
 
-  ngOnInit(): void {
-    
-    const submitBtn = document.querySelector('.submit-btn') as HTMLInputElement;
-    submitBtn.addEventListener('click', this.onSubmit);
-  }
 
-  onSubmit(event: Event) {
-    event.preventDefault(); 
-    
-    console.log('Form submitted!');
-    
-    const nameInput = document.getElementById('name') as HTMLInputElement;
-    console.log('Name:', nameInput.value);
-    
+export class ContactComponent {
+  name: string = '';
+  email: string = '';
+  message: string = '';
+
+
+  constructor(private http: HttpClient) {}
+
+  //  Form subsmission  //
+  
+  onSubmit(contactForm: NgForm) {
+    const formData = {
+      name: this.name,
+      email: this.email,
+      message: this.message
+    };
+
+
+    this.http.post('http://bla/bla/bla', formData)
+      .subscribe({
+        next: (response) => {
+          console.log('Form submission successful!', response);
+          contactForm.resetForm();
+        },
+
+        error: (error) => {
+          console.error('Error submitting form:', error);
+        }
+      });
   }
 }

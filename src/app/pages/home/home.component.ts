f79a35e98691cf708d8e9d1d909ce6e51c2c859d
Kostas <diamantopoulos.kostas@hotmail.com>
2024-06-17 19:21:41 +0100

    changes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,17 @@
 import { Component, } from '@angular/core';
 import { LazyLoadImageModule } from 'ng-lazyload-image';
 import 'src/styles.css'
+import { FooterComponent } from "../../layout/footer/footer/footer.component";
 
 
 
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [LazyLoadImageModule],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [LazyLoadImageModule, FooterComponent]
 })
 export class HomeComponent {
 

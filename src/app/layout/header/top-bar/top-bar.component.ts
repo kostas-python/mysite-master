7dfbc33e467f0e01e88d7a66b2f68305d14c8cd5
Kostas <diamantopoulos.kostas@hotmail.com>
2024-07-20 13:18:25 +0100

    added common module to use ngif

--- src/app/layout/header/top-bar/top-bar.component.ts
+++ src/app/layout/header/top-bar/top-bar.component.ts
@@ -1,13 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterLink, RouterModule } from '@angular/router';
 import { ContactComponent } from 'src/app/pages/contact/contact.component';
 
 
-
 @Component({
   selector: 'app-top-bar',
   standalone: true,
-  imports: [RouterLink, RouterModule, ContactComponent],
+  imports: [RouterLink, RouterModule, ContactComponent,CommonModule],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.css'
 })
